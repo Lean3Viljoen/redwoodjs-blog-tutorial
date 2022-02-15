aecdf1eb884443ea96c85e245d900162df23b09e
Leandri Nieuwoudt <leandri@mohara.co>
2022-02-15 21:46:14 +0200

    add mutation, add db validation

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -2,22 +2,50 @@ import { MetaTags } from '@redwoodjs/web'
 import {
   Form,
   FieldError,
+  FormError,
   Label,
   TextAreaField,
   TextField,
   Submit,
+  useForm,
 } from '@redwoodjs/forms'
 
+import { useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
 const ContactPage = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission!')
+      formMethods.reset()
+    },
+  })
+
   const onSubmit = (data) => {
-    console.log(data)
+    create({ variables: { input: data } })
   }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <h1>ContactPage</h1>
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+      <Toaster toastOptions={{ duration: 100000 }} />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
         <Label name="name" errorClassName="error">
           Name
         </Label>
@@ -53,7 +81,7 @@ const ContactPage = () => {
           name="message"
         />
         <FieldError className="error" name="message" />
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </>
   )
